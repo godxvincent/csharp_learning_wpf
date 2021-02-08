675df5e8960ab15976c7e5779d6bff0a97f560dd
godxvincent <godxvincent@gmail.com>
2021-02-08 08:54:09 -0500

    :sparkles: Testing wrap panel and using c# to add visual elements

--- WPF_Basics/MainWindow.xaml.cs
+++ WPF_Basics/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace WPF_Basics
         public MainWindow()
         {
             InitializeComponent();
+            // Esto sobre escribe lo que este definido en el XAML file.
+            // Aqui podemos agregar nuevos componentes.
+            Grid grid = new Grid();
+            // Agregamos una grid a la ventana.
+            this.Content = grid;
+            Button btn = new Button();
+            btn.Content = "Button Created with C#";
+            btn.FontSize = 16;
+            btn.Height = 30;
+            btn.Width = 300;
+
+            WrapPanel wrapPanel = new WrapPanel();
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Contenido 1";
+            textBlock.Foreground = Brushes.AliceBlue;
+            wrapPanel.Children.Add(textBlock);
+
+            textBlock = new TextBlock();
+            textBlock.Text = "Contenido 2";
+            textBlock.Foreground = Brushes.Aquamarine;
+            wrapPanel.Children.Add(textBlock);
+
+            textBlock = new TextBlock();
+            textBlock.Text = "Contenido 3";
+            textBlock.Foreground = Brushes.Beige;
+            wrapPanel.Children.Add(textBlock);
+
+            btn.Content = wrapPanel;
+
+            grid.Children.Add(btn);
+
         }
     }
 }
