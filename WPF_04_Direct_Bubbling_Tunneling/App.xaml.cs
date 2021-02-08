98fd2e7c566f02b4ca8dbed97675ad56856db3c3
godxvincent <godxvincent@gmail.com>
2021-02-08 10:48:05 -0500

    :sparkles: Working with c# events

---
+++ WPF_04_Direct_Bubbling_Tunneling/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_04_Direct_Bubbling_Tunneling
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WPF_04_Direct_Bubbling_Tunneling/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_04_Direct_Bubbling_Tunneling
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // El sender es una referencia al objeto que genera el evento.
+            MessageBox.Show("Button was clicked - Direct Event");
+        }
+
+        // Los eventos bubbling van al final de la jerarquia de eventos en el arbol de componentes.
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse Button went up / was released - Bubbling Event");
+        }
+
+        // Los eventos tunneling van al inicio de la jerarquia de eventos en el arbol de componentes.
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse Button went up / was released - Tunneling Event");
+        }
+
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse Button_PreviewMouseLeftButtonDown");
+        }
+
+        private void Button_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse Button_PreviewMouseRightButtonDown");
+        }
+    }
+}
