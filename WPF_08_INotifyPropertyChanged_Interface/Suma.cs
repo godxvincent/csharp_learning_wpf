e09a1c96693cd743258e8dfc9b9ea51bcf5aa92a
godxvincent <godxvincent@gmail.com>
2021-02-09 22:04:53 -0500

    :construction: Working with listbox and databinding.

---
+++ WPF_08_INotifyPropertyChanged_Interface/Suma.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_08_INotifyPropertyChanged_Interface
+{
+    public class Suma : System.ComponentModel.INotifyPropertyChanged
+    {
+
+        private string num1;
+        private string num2;
+        private string resultado;
+
+        public string Num1 {
+            get { return num1; }
+            set {
+                int numero;
+                bool esNumerico = int.TryParse(value, out numero);
+                num1 = esNumerico ? value : "";
+                OnPropertyChanged("Num1");
+                OnPropertyChanged("Resultado");
+            }
+        }
+
+        public string Num2
+        {
+            get { return num2; }
+            set
+            {
+                int numero;
+                bool esNumerico = int.TryParse(value, out numero);
+                num2 = esNumerico ? value : "";
+                OnPropertyChanged("Num2");
+                OnPropertyChanged("Resultado");
+            }
+        }
+
+        public string Resultado
+        {
+            get { return (int.Parse(num1) + int.Parse(num2)).ToString(); }
+            set
+            {
+                resultado = (int.Parse(num1) + int.Parse(num2)).ToString();
+                OnPropertyChanged("Resultado");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ WPF_09_ListBox_ListCurrentMatches/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_09_ListBox_ListCurrentMatches
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid",  Score1 = 3, Score2 = 2, Completion=85});
+            matches.Add(new Match() { Team1 = "PSG", Team2 = "Barca",  Score1 = 1, Score2 = 2, Completion=35});
+            matches.Add(new Match() { Team1 = "BVB Dortmund", Team2 = "AS Roma",  Score1 = 2, Score2 = 2, Completion=15});
+            matches.Add(new Match() { Team1 = "Man United", Team2 = "La Galaxy",  Score1 = 5, Score2 = 3, Completion=45});
+            // Aqui agregamos la lista al listbox
+            lbMatches.ItemsSource = matches;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbMatches.SelectedItem != null)
+            {
+                var matchSelected = (lbMatches.SelectedItem as Match);
+                MessageBox.Show(
+                    "Selected Match => (" +
+                    matchSelected.Team1 +
+                    " [" +
+                    matchSelected.Score1 +
+                    "-" +
+                    matchSelected.Score2 +
+                    "] " +
+                    matchSelected.Team2 +
+                    ") Completion " +
+                    matchSelected.Completion
+                    );
+            } else
+            {
+                MessageBox.Show("Por favor seleccione un match");
+            }
+        }
+    }
+
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Completion { get; set; }
+
+    }
+}
